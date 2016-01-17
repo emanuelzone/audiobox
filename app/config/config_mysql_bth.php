4e39ded154d12f6b52fdb2cadd367507710242f3
emanulzone <martin@emanuelzone.se>
2016-01-17 12:04:54 +0100

    Update config_mysql_bth.php

--- app/config/config_mysql_bth.php
+++ app/config/config_mysql_bth.php
@@ -1,11 +1,1 @@
-<?php
 
-return [
-    'dsn'             => "dsn?",
-    'username'        => "username?",
-    'password'        => "password?",
-    'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
-    'table_prefix'    => 'phpmvc_project_',
-    'verbose' 		  => false,
-    //'debug_connect' => 'true',
-];
