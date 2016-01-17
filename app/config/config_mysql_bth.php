f938af24afd6428589a93e5b39c7217363b43d98
emanulzone <martin@emanuelzone.se>
2016-01-17 12:03:19 +0100

    Update config_mysql_bth.php

--- app/config/config_mysql_bth.php
+++ app/config/config_mysql_bth.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'dsn'             => "mysql:host=blu-ray.student.bth.se;dbname=maem14;",
-    'username'        => "maem14",
-    'password'        => "97x20g\A",
+    'dsn'             => "dsn?",
+    'username'        => "username?",
+    'password'        => "password?",
     'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
     'table_prefix'    => 'phpmvc_project_',
     'verbose' 		  => false,
